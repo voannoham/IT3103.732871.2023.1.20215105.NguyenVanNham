323709a8dfe06730aff8e5249a4c08084ba0b21a
voannoham <129341222+voannoham@users.noreply.github.com>
2023-10-16 09:49:50 +0700

    Add files via upload

---
+++ ShowTwoNumbers.java
@@ -0,0 +1,23 @@
+//Nguyen Van Nham 20215105
+//Example 5: ShowTwoNumbers.java
+import javax.swing.JOptionPane;
+public class ShowTwoNumbers{
+    public static void main(String[] args){
+        String strNum1, strNum2;
+        String strNotification = "Nguyen Van Nham 20215105-you've just enterd: ";
+	
+	strNum1 = JOptionPane.showInputDialog(null,
+        	"Nguyen Van Nham-5105-Please input the first number: ","Input the first number",
+		JOptionPane.INFORMATION_MESSAGE);
+	strNotification += strNum1 + " and ";
+	
+	strNum2 = JOptionPane.showInputDialog(null,
+		"Nguyen Van Nham-5105-Please input the second number: ","Input the second number",
+		JOptionPane.INFORMATION_MESSAGE);
+	strNotification += strNum2;
+
+	JOptionPane.showMessageDialog(null,strNotification,
+		"Nguyen Van Nham 20215105-Show two numbers", JOptionPane.INFORMATION_MESSAGE);
+	System.exit(0);
+    }
+}
